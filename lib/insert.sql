85b2f521406d2398cc75597c2bf2e4070053dcb7
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-20 19:39:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
-INSERT INTO bears VALUES ();
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "female", "brown", "nasty", );
+INSERT INTO bears VALUES (2);
+INSERT INTO bears VALUES (3);
+INSERT INTO bears VALUES (4);
+INSERT INTO bears VALUES (5);
+INSERT INTO bears VALUES (6);
+INSERT INTO bears VALUES (7);
+INSERT INTO bears VALUES (8);
