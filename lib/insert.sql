139f5330bd3319859bdc62b96f584634abd302fa
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-20 20:34:01 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "male", "brown", "cuddly", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "male", "black", "playful", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 1, "female", "white", "sweet", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 8, "female", "brown", "serious", 1);
+INSERT INTO bears VALUES (5, "Melissa", 15, "female", "tan", "friendly", 1);
+INSERT INTO bears VALUES (6, "Grinch", 7, "female", "tan", "cranky", 1);
+INSERT INTO bears VALUES (7, "Wendy", 3, "female", "red", "sleepy", 1);
+INSERT INTO bears VALUES (8, NULL, 3, "male", "brown", "nasty", 1);
