44c5149301d1c97c1d16841081c84fb32586e091
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-20 19:42:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "male", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "male", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 1, "female", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 8, "female", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 15, "female", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 7, "female", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "male", "brown", "nasty", 1);
+INSERT INTO bears VALUES (1, "Rowdy", 10, "male", "black", "playful", 1);
+INSERT INTO bears VALUES (1, "Tabitha", 1, "female", "white", "sweet", 1);
+INSERT INTO bears VALUES (1, "Sergeant Brown", 8, "female", "brown", "serious", 1);
+INSERT INTO bears VALUES (1, "Melissa", 15, "female", "tan", "friendly", 1);
+INSERT INTO bears VALUES (1, "Grinch", 7, "female", "tan", "cranky", 1);
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "male", "golden", "nasty", 1);
+INSERT INTO bears VALUES (1, "Wendy", 3, "female", "red", "sleepy", 1);
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "female", "brown", "nasty", 1);
