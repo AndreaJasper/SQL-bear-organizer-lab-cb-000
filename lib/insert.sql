70f09eaab9bf141bc113a473ff672231c4a44631
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-20 19:41:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears VALUES (1, "Mr. Chocolate", 1, "female", "brown", "nasty", 1);
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 8, "female", "brown", "nasty", 1);
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 15, "female", "brown", "nasty", 1);
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 7, "female", "brown", "nasty", 1);
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "female", "brown", "nasty", 1);
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "male", "brown", "nasty", 1);
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "female", "brown", "nasty", 1);
